1fdb20e83840b17ccfaf0fbdb6e4d82d5369d746
tiendd <tiendd@fabbi.io>
2020-06-26 13:20:12 +0700

    commit2

--- config/settings.py
+++ config/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = 'Book-management/static'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
